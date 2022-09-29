0346200fb7029b51a405c354766a8eeb81227b96
alejandroromon <alejandroromeromontero@gmail.com>
2022-09-29 13:13:55 +0200

    Corrección domain name generator

--- Domain-name-generator/src/app.js
+++ Domain-name-generator/src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".es"];
+  let print = "";
+  let aux = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let h = 0; h < domain.length; h++) {
+          print += `${pronoun[i] + adj[j] + noun[k] + domain[h]}</br>`;
+        }
+      }
+    }
+  }
+  console.log(print);
+  document.getElementById("mensaje").innerHTML = print;
   console.log("Hello Rigo from the console!");
 };
